afb8b54c5ef0da5c66da325787a2157c882cc148
dkeum <dkeum@sfu.ca>
2023-10-17 17:21:34 -0700

    updating

--- node-express-vercel-master/index.js
+++ node-express-vercel-master/index.js
@@ -1,28 +1,37 @@
 import express from "express";
 import 'dotenv/config';
 import cors from "cors";
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-let db; 
-const mongodb = async () => {
-    const client = new MongoClient(`${process.env.MONGODB_URI}`);
-    await client.connect();
-    db = client.db("Pokedex");
-    console.log("Successfully connected to Database")
+const connectionString = "mongodb+srv://vercel-admin-user:PQqaEIYhj9qMY5U3@pokedex.hdijcp3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(connectionString);
+let conn;
+try {
+  conn = await client.connect();
+} catch(e) {
+  console.error(e);
 }
+let db = conn.db("Pokedex");
+
+
 const app = express();
-app.use(cors(
-    {
-        origin: ["https://pokemonencyclopedia.vercel.app"],
-        methods: ["POST","PUT","GET"],
-        credentials: true
-    }
-));
+// app.use(cors(
+//     {
+//         origin: ["https://pokemonencyclopedia.vercel.app"],
+//         methods: ["POST","PUT","GET"],
+//         credentials: true
+//     }
+// ));
 
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send("Api is working");
+app.get('/', async (req,res)=>{
+    let collection = await db.collection("pokemonInfo");
+    let results = await collection.find({})
+    .limit(50)
+    .toArray();
+     res.send(results).status(200);
+     res.send("Api is working");
 })
 
 //api/PokemonEncyclopedia_v1/pokemonencyclopedia/:pokemonId/
@@ -33,6 +42,7 @@ app.get('/api/:pokemonId/', async (req,res)=>{
     pokemonId = parseInt(pokemonId);
 
     try{
+
         const pokemon = await db.collection('pokemonInfo').findOne({pokemonId});
         if (pokemon) {
             // const upvoteIds = pokemon.upvotes || [];
@@ -49,66 +59,64 @@ app.get('/api/:pokemonId/', async (req,res)=>{
 
 });
 
-app.put('/api/PokemonEncyclopedia_v1/pokemonencyclopedia/:pokemonId/upvote', async (req,res)=>{
-    let {pokemonId} = req.params;
+// app.put('/api/PokemonEncyclopedia_v1/pokemonencyclopedia/:pokemonId/upvote', async (req,res)=>{
+//     let {pokemonId} = req.params;
 
-    // const pokemon = await db.collection('pokemonInfo')
-    pokemonId = parseInt(pokemonId);
+//     // const pokemon = await db.collection('pokemonInfo')
+//     pokemonId = parseInt(pokemonId);
 
-    const pokemon = await db.collection('pokemonInfo').findOne({pokemonId});
+//     const pokemon = await db.collection('pokemonInfo').findOne({pokemonId});
 
-    if(pokemon){
-        await db.collection('pokemonInfo').updateOne({pokemonId}, 
-        {
-            $inc: { upvotes: 1 }
-        });
-        const updatedPokemon = await db.collection('pokemonInfo').findOne({ pokemonId });
-        res.json(updatedPokemon);
-    }
-    else{
-        res.send("Pokemon doesn't exist").status(404);
-    }
-});
+//     if(pokemon){
+//         await db.collection('pokemonInfo').updateOne({pokemonId}, 
+//         {
+//             $inc: { upvotes: 1 }
+//         });
+//         const updatedPokemon = await db.collection('pokemonInfo').findOne({ pokemonId });
+//         res.json(updatedPokemon);
+//     }
+//     else{
+//         res.send("Pokemon doesn't exist").status(404);
+//     }
+// });
 
-app.post('/api/PokemonEncyclopedia_v1/pokemonencyclopedia/:pokemonId/comments', async (req,res)=>{
+// app.post('/api/PokemonEncyclopedia_v1/pokemonencyclopedia/:pokemonId/comments', async (req,res)=>{
 
-    let {pokemonId} = req.params;
-    pokemonId = parseInt(pokemonId);
-    const {postedBy, text} = req.body;
+//     let {pokemonId} = req.params;
+//     pokemonId = parseInt(pokemonId);
+//     const {postedBy, text} = req.body;
 
-    await db.collection('pokemonInfo').updateOne({ pokemonId }, {
-        $push: { comments: { postedBy, text}  },
-    });
+//     await db.collection('pokemonInfo').updateOne({ pokemonId }, {
+//         $push: { comments: { postedBy, text}  },
+//     });
 
-    const pokemon = await db.collection('pokemonInfo').findOne({ pokemonId });
+//     const pokemon = await db.collection('pokemonInfo').findOne({ pokemonId });
 
-    if(pokemon){
-        res.json(pokemon);
-    }
-    else{
-        try{
-        const newPokemon = {
-            pokemonId,
-            upvotes: 0,
-            comments: [{ postedBy, text }],
-        };
-        await db.collection('pokemonInfo').insertOne(newPokemon);
-        res.json(newPokemon);
-        }
-        catch(e) {
-            console.error(error);
-            res.status(500).send("Internal Server Error");
-        }
-    }
+//     if(pokemon){
+//         res.json(pokemon);
+//     }
+//     else{
+//         try{
+//         const newPokemon = {
+//             pokemonId,
+//             upvotes: 0,
+//             comments: [{ postedBy, text }],
+//         };
+//         await db.collection('pokemonInfo').insertOne(newPokemon);
+//         res.json(newPokemon);
+//         }
+//         catch(e) {
+//             console.error(error);
+//             res.status(500).send("Internal Server Error");
+//         }
+//     }
 
     
-});
+// });
 
 
 const PORT = process.env.PORT || 8000; 
-
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
-    db = mongodb(); 
 });
 
